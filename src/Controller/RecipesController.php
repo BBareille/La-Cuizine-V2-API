9d62a6a1ec7c123d4aa392f7028708adf46bc68c
BaptisteB <baptiste.bareille@outlook.com>
2023-02-12 18:40:41 +0100

    Add CRUD on recipe, data is passed in request body, need to do it for ingredient too

--- src/Controller/RecipesController.php
+++ src/Controller/RecipesController.php
@@ -4,12 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Recipes;
 use App\Repository\RecipesRepository;
-use http\Client\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Tests\Compiler\J;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class RecipesController extends AbstractController
 {
@@ -23,16 +24,56 @@ class RecipesController extends AbstractController
     }
 
     #[Route('/api/newRecipes/', name: 'newIngredient', methods: ['POST'])]
-    public function newIngredient(Request $request,RecipesRepository $recipesRepository, SerializerInterface $serializer): JsonResponse
+    public function newRecipe(Request $request,RecipesRepository $recipesRepository, SerializerInterface $serializer): JsonResponse
     {
-        $test = $request->getBody();
-        $jsontest = $serializer->serialize($test, 'json');
-//        $newRecipes = new Recipes();
-//        $newRecipes->setTitle($name);
-//        $newRecipes->setUserId(1);
-//        $recipesRepository->save($newRecipes, true);
-//        $jsonNewRecipes = $serializer->serialize($newRecipes, 'json');
+          $data = json_decode($request->getContent(), true);
+          $newRecipes = new Recipes();
+        $newRecipes
+            ->setTitle($data['title'])
+            ->setImage($data['image'])
+            ->setCookingTime($data['cooking_time'])
+            ->setPrepTime($data['prep_time']);
 
-        return new JsonResponse($jsontest, Response::HTTP_OK, [], true);
+        $newRecipes->setUserId(1);
+        $recipesRepository->save($newRecipes, true);
+        $jsonData = $serializer->serialize($data, 'json');
+
+        return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
     }
+
+      #[Route('/api/deleteRecipe/{id}', name: 'deleteIngredient', methods: ['DELETE'])]
+      public function deleteRecipe(int $id, RecipesRepository $recipesRepository, SerializerInterface $serizalizer){
+
+            $recipeToDelete = $recipesRepository->findOneBy(["id"=>$id]);
+            $recipesRepository->remove($recipeToDelete, true);
+            $jsonRecipeToDelete = $serizalizer->serialize($recipeToDelete, 'json');
+
+
+            return new JsonResponse($jsonRecipeToDelete, Response::HTTP_OK, [], true);
+      }
+
+      #[Route('api/recipe/{id}', name:'getOneRecipe', methods: ['GET'])]
+      public function getOneRecipebyID(int $id, RecipesRepository $recipesRepository, SerializerInterface $serializer){
+          $recipe = $recipesRepository->findOneBy(['id'=>$id]);
+          $jsonRecipe = $serializer->serialize($recipe, 'json');
+
+          return new JsonResponse($jsonRecipe, Response::HTTP_OK, [], true);
+      }
+
+      #[Route('api/modifyRecipe/{id}', name:'modifyRecipe', methods: ['PUT'])]
+      public function modifyRecipe(int $id,Request $request, RecipesRepository $recipesRepository, SerializerInterface $serializer){
+            $recipeToUpdate = $recipesRepository->findOneBy(["id"=>$id]);
+            $data = json_decode($request->getContent(), true);
+            $recipeToUpdate
+                ->setTitle($data['title'])
+                ->setImage($data['image'])
+                ->setCookingTime($data['cooking_time'])
+                ->setPrepTime($data['prep_time']);
+
+            $recipeToUpdate->setUserId(1);
+            $recipesRepository->save($recipeToUpdate, true);
+            $jsonData = $serializer->serialize($data, 'json');
+
+            return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+      }
 }
