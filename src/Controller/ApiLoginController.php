b5635f342f502cdb620b052c83f73229d4711ef9
Baptiste <baptiste.bareille@outlook.com>
2023-02-20 17:00:52 +0100

    adding token to log

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -1,27 +1,15 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\User;
-use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class ApiLoginController extends AbstractController
 {
     #[Route('/api/login', name: 'api_login')]
-    public function index(#[CurrentUser] ?User $user): Response
+    public function index(): Response
     {
-        if (null === $user){
-                return $this->json([
-                            'message' => 'missing Credentials',
-                ]);
-        }
-        
-        return $this->json([
-                    'user' => $user->getUserIdentifier(),
-                'message' => 'Connexion réussie'
-        ]);
     }
+    
 }
